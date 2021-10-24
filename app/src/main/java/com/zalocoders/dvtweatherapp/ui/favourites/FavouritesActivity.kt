33afcdafd9ce42d33748f2aba2a770f5440e8c76
Zalo Austine <austinezaloa@gmail.com>
2021-10-24 23:44:17 +0300

    navigations

--- app/src/main/java/com/zalocoders/dvtweatherapp/ui/favourites/FavouritesActivity.kt
+++ app/src/main/java/com/zalocoders/dvtweatherapp/ui/favourites/FavouritesActivity.kt
@@ -2,6 +2,12 @@ package com.zalocoders.dvtweatherapp.ui.favourites
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.zalocoders.dvtweatherapp.R
 import com.zalocoders.dvtweatherapp.databinding.ActivityFavouritesBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,10 +18,28 @@ class FavouritesActivity : AppCompatActivity() {
 		ActivityFavouritesBinding.inflate(layoutInflater)
 	}
 	
+	private lateinit var appBarConfiguration:AppBarConfiguration
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(binding.root)
+		
+		val navHostFragment =
+				supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+		val navController = navHostFragment.navController
+		 appBarConfiguration = AppBarConfiguration(
+				topLevelDestinationIds = setOf(),
+				fallbackOnNavigateUpListener = ::onSupportNavigateUp
+		)
+		
+		setupActionBarWithNavController(navController, appBarConfiguration)
 	}
 	
+	
+	override fun onSupportNavigateUp(): Boolean {
+		val navController = findNavController(R.id.nav_host_fragment)
+		return navController.navigateUp(appBarConfiguration)
+				|| super.onSupportNavigateUp()
+	}
 	
 }
